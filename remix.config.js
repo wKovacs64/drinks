34ff5d8c50e33f46ca149a8f4544392a9d92e74c
Justin Hall <justin.r.hall@gmail.com>
2022-06-25 15:04:02 -0600

    refactor: bundle p-throttle on the server

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   // I don't know why this is needed, but live reloads didn't work without it.
   devServerPort: 8002,
   ignoredRouteFiles: ['**/.*'],
-  serverDependenciesToBundle: ['marked'],
+  serverDependenciesToBundle: ['marked', 'p-throttle'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",

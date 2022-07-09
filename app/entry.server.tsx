ddd477911500356e6bf2ec5bdce061ad79db032a
Justin Hall <justin.r.hall@gmail.com>
2022-07-09 14:35:22 -0600

    chore: temporarily add request time logging

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -8,12 +8,14 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
+  console.time('handleRequest');
   let markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />,
   );
 
   responseHeaders.set('Content-Type', 'text/html');
 
+  console.timeEnd('handleRequest');
   return new Response('<!DOCTYPE html>' + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
@@ -25,6 +27,7 @@ export const handleDataRequest: HandleDataRequestFunction = async (
   response,
   { request },
 ) => {
+  console.time('handleDataRequest');
   const isGet = request.method.toLowerCase() === 'get';
   const purpose =
     request.headers.get('Purpose') ||
@@ -39,5 +42,6 @@ export const handleDataRequest: HandleDataRequestFunction = async (
     response.headers.set('Cache-Control', 'private, max-age=10');
   }
 
+  console.timeEnd('handleDataRequest');
   return response;
 };

d5781a0ffc2bf62d36462c99ccc8454aa08332e0
Justin Hall <justin.r.hall@gmail.com>
2022-07-19 18:36:06 -0600

    refactor: touch up requestIdleCallbackShim implementation

--- app/utils/request-idle-callback-shim.ts
+++ app/utils/request-idle-callback-shim.ts
@@ -1,8 +1,8 @@
 // TODO: remove once requestIdleCallback is available in Safari
 // https://caniuse.com/requestidlecallback
 export function requestIdleCallbackShim(cb: () => void) {
-  if (typeof window !== 'undefined' && 'requestIdleCallback' in window) {
+  if ('requestIdleCallback' in window) {
     return window.requestIdleCallback(cb);
   }
-  return setTimeout(cb, 0);
+  return window.setTimeout(cb, 1);
 }

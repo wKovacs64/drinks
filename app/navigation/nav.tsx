6aa52be208bed109fad4e34678fa33da0b86a92a
Justin Hall <justin.r.hall@gmail.com>
2022-07-31 18:48:47 -0600

    fix: increase space below breadcrumbs

--- app/navigation/nav.tsx
+++ app/navigation/nav.tsx
@@ -1,5 +1,5 @@
 export default function Nav({ children }: { children: React.ReactNode }) {
   return (
-    <nav className="mb-4 px-4 text-gray-100 sm:mb-8 sm:p-0">{children}</nav>
+    <nav className="mb-6 px-4 text-gray-100 sm:mb-8 sm:p-0">{children}</nav>
   );
 }

d7a264750c9e505a09667f640bed5c9b5e0e6f8c
Justin Hall <justin.r.hall@gmail.com>
2022-07-31 18:40:05 -0600

    chore: remove server entry debug logging

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -8,14 +8,12 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  console.time('handleRequest');
   let markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />,
   );
 
   responseHeaders.set('Content-Type', 'text/html');
 
-  console.timeEnd('handleRequest');
   return new Response('<!DOCTYPE html>' + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
@@ -27,7 +25,6 @@ export const handleDataRequest: HandleDataRequestFunction = async (
   response,
   { request },
 ) => {
-  console.time('handleDataRequest');
   const isGet = request.method.toLowerCase() === 'get';
   const purpose =
     request.headers.get('Purpose') ||
@@ -42,6 +39,5 @@ export const handleDataRequest: HandleDataRequestFunction = async (
     response.headers.set('Cache-Control', 'private, max-age=10');
   }
 
-  console.timeEnd('handleDataRequest');
   return response;
 };

4f9ba2ce691562f22f98a0449c847b2db231bb50
Justin Hall <justin.r.hall@gmail.com>
2023-03-13 14:16:10 -0600

    refactor: enable Tailwind via PostCSS so we can also get Autoprefixer

--- remix.config.js
+++ remix.config.js
@@ -11,7 +11,7 @@ module.exports = {
   future: {
     unstable_cssSideEffectImports: true,
     unstable_dev: true,
-    unstable_tailwind: true,
+    unstable_postcss: true,
     v2_errorBoundary: true,
     v2_routeConvention: true,
   },

36bc80e8fee335ddbe336f1fc40bcb5978880196
Justin Hall <justin.r.hall@gmail.com>
2023-04-04 13:14:03 -0600

    chore: enable v2_normalizeFormMethod in Remix config

--- remix.config.js
+++ remix.config.js
@@ -13,6 +13,8 @@ module.exports = {
     unstable_dev: true,
     unstable_postcss: true,
     v2_errorBoundary: true,
+    // v2_meta: true,
+    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
 };

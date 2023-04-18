b61ca0b3578d3158eadea4f56ffff8d642f5592d
Justin Hall <justin.r.hall@gmail.com>
2023-04-18 15:48:00 -0600

    perf: provide metric overrides for fallback font to avoid CLS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
@@ -11,7 +9,7 @@ module.exports = {
         cream: '#eedebf', // closest: orange-200
       },
       fontFamily: {
-        sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+        sans: ['Source Sans Pro', 'Source Sans Pro Fallback'],
       },
       transitionTimingFunction: {
         default: 'ease',

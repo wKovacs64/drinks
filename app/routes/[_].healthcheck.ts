cc9c959875e99fae63252a314fbf061b83677c0e
Justin Hall <justin.r.hall@gmail.com>
2023-08-02 13:36:48 -0600

    refactor: update healthcheck logic

--- app/routes/[_].healthcheck.ts
+++ app/routes/[_].healthcheck.ts
@@ -1,11 +1,21 @@
+import type { LoaderArgs } from '@remix-run/node';
 import { prisma } from '~/utils/db.server';
 
-export const loader = async () => {
+export async function loader({ request }: LoaderArgs) {
+  const host =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host');
+  const url = new URL('/', `http://${host}`);
+
   try {
-    await prisma.cacheEntry.count();
-    return new Response(null, { status: 200 });
+    await Promise.all([
+      prisma.cacheEntry.count(),
+      fetch(url.toString()).then((response) => {
+        if (!response.ok) return Promise.reject(response);
+      }),
+    ]);
+    return new Response('OK', { status: 200 });
   } catch (error: unknown) {
     console.log('❌ Healthcheck failed', { error });
-    return new Response(null, { status: 500 });
+    return new Response('ERROR', { status: 500 });
   }
-};
+}

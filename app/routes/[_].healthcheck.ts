8df1ba62948f28c728a8b468d828ffab598ca76c
Justin Hall <justin.r.hall@gmail.com>
2025-06-19 17:51:56 -0600

    fix: eliminate self-request in healthcheck endpoint
    I think this HEAD request that Fly hits every ~5 seconds was causing the
    index loader to run, querying Contentful for all drink data and likely
    even requesting all the images. 🤦‍♂️

--- app/routes/[_].healthcheck.ts
+++ app/routes/[_].healthcheck.ts
@@ -1,19 +1,3 @@
-import { invariant } from '@epic-web/invariant';
-import type { Route } from './+types/[_].healthcheck';
-
-export async function loader({ request }: Route.LoaderArgs) {
-  const host = request.headers.get('X-Forwarded-Host') || request.headers.get('host');
-  const url = new URL('/', `http://${host}`);
-
-  try {
-    const response = await fetch(url.toString(), {
-      method: 'HEAD',
-      headers: { 'x-from-healthcheck': 'true' },
-    });
-    invariant(response.ok, `HEAD request failed to ${url}`);
-    return new Response('OK', { status: 200 });
-  } catch (error) {
-    console.log('❌ Healthcheck failed', { error });
-    return new Response('ERROR', { status: 500 });
-  }
+export async function loader() {
+  return new Response('OK', { status: 200 });
 }

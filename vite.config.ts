d8f08bb053ee0413564b9faf0d2abdf815847325
Justin Hall <justin.r.hall@gmail.com>
2025-10-18 11:54:26 -0600

    chore: enable React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, normalizePath } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { iconsSpritesheet } from 'vite-plugin-icons-spritesheet';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
   build: {
@@ -27,6 +28,15 @@ export default defineConfig({
       withTypes: true,
     }),
     reactRouter(),
+    babel({
+      exclude: ['**/node_modules/**'],
+      filter: /\.[jt]sx?$/u,
+      loader: 'jsx',
+      babelConfig: {
+        presets: ['@babel/preset-typescript'],
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
     viteStaticCopy({
       targets: [
         {

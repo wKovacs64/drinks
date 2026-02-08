36f4583f476793622b6d28a2e49d1529a9ce9c97
Justin Hall <justin.r.hall@gmail.com>
2026-02-08 12:10:19 -0700

    refactor: use recommended Tailwind classes in header

--- app/core/header.tsx
+++ app/core/header.tsx
@@ -4,7 +4,7 @@ import { Icon } from '#/app/icons/icon';
 export function Header() {
   return (
     <header className="flex flex-col items-center bg-[#111111] p-4 text-stone-300 md:p-8">
-      <section className="flex w-full flex-wrap items-center justify-between sm:w-[26rem] lg:w-full lg:max-w-[60rem] xl:max-w-[80rem]">
+      <section className="flex w-full flex-wrap items-center justify-between sm:w-104 lg:w-full lg:max-w-240 xl:max-w-7xl">
         {/* TODO: change to h2 or something, move h1 to interesting page content */}
         <h1 className="text-3xl font-light">
           <HeaderLink to={href('/')}>drinks.fyi</HeaderLink>

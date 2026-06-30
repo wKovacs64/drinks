9debb924acf39b76a174bdaad08bed3b2b62e606
Justin Hall <justin.r.hall@gmail.com>
2026-06-29 18:49:08 -0600

    chore(deps): remove RR v8 future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,11 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   future: {
     unstable_optimizeDeps: true,
-    v8_middleware: true,
-    v8_passThroughRequests: true,
-    v8_splitRouteModules: true,
-    v8_trailingSlashAwareDataRequests: true,
-    v8_viteEnvironmentApi: true,
   },
   ssr: true,
 } satisfies Config;
